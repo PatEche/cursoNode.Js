adcd7c3b7db43bd9854d2650d7a7828385eca847
PatEche <echeverriaple@hotmail.com>
2023-06-24 12:48:19 -0300

    Nuevas expresiones

--- 06-operadores-expresiones/re.js
+++ 06-operadores-expresiones/re.js
@@ -1,3 +1,12 @@
-const re = /.hola./
+// const re = /.hola./
 
-console.log(re.test(" hola "));
+// console.log(re.test(" hola "));
+
+const re = /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/;
+function testInfo(phoneInput) {
+  var OK = re.exec(phoneInput.value);
+  if (!OK) {
+    alert(phoneInput.value + ' No es un numero telefonico valido!');
+  } else {
+    alert('Gracias, tu número de teléfono es ' + OK[0]);}
+}
