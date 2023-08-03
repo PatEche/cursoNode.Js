e054be73cf6829c2c7c6a0379ef9d07a1db226ff
PatEche <echeverriaple@hotmail.com>
2023-08-03 18:28:00 -0300

    Nuevos Cambios

--- 06-operadores-expresiones/re.js
+++ 06-operadores-expresiones/re.js
@@ -10,3 +10,21 @@ function testInfo(phoneInput) {
   } else {
     alert('Gracias, tu número de teléfono es ' + OK[0]);}
 }
+
+
+
+//Defino dentro de bloque y solo es accesible desde adentro de el.
+// {
+//   let clima = "nublado";
+//   console.log(clima);
+// }
+
+// console.log(clima);
+
+// let edad = 14;
+// let sabeConducir = true;
+
+// if(edad >=18 && sabeConducir === true ){
+//   console.log("Puede conducir");
+// }else
+//   console.log("No puedes conducir");
